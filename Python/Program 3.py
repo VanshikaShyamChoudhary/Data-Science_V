18231e22acfa2924c4a24affece222d474279c10
VANSHIKA <vanshika12.sc@gmail.com>
2026-08-08 13:06:27 +0530

    Delete Python/Program 3.py

--- Python/Program 3.py
+++
@@ -1,9 +0,0 @@
-name="Vanshika" 
-age=20
-price= 20.55
-
-
-print (type(name)) # to print the type of variable, name is
-print (type(age)) # to prinnt type of age
-print (type(price))# to print the type of price
-
