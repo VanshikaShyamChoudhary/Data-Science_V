2803c059804b4770780726fb94b2f4688c400c4c
VANSHIKA <vanshika12.sc@gmail.com>
2026-08-08 13:05:35 +0530

    Add files via upload

---
+++ Python/Lecture 1/Program 2.py
@@ -0,0 +1,7 @@
+name="Vanshika " #string 
+age=20 # double quotes is not required as it is an integer
+price= 20.55
+
+print("my name is:",name)
+print("my age is :",age)
+
---
+++ Python/Lecture 1/Program 3.py
@@ -0,0 +1,9 @@
+name="Vanshika" 
+age=20
+price= 20.55
+
+
+print (type(name)) # to print the type of variable, name is
+print (type(age)) # to prinnt type of age
+print (type(price))# to print the type of price
+
